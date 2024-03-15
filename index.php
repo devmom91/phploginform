91b7cd5d8390f605e2c8817beb345e7de2c0da49
devmom91 <devmom91@emetang91@gmail.com>
2024-03-15 09:21:59 -0400

    phploginform

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div id= "form">
+        <h1> Login form </h1>
+        <form name="form" method="POST">
+        <label> Username: </label>
+        <input type="text" id="user" name="user"> </br></br>
+        <label>Password:</label>
+        <input type"Password" id="pass" name="pass">
+          </br></br> 
+        <input type="submit" id="btn" value="Login"  name="submit"/> 
+        <value="Login" style="text-align:center;"/>
+    </div> 
+</body>
+</html>
